1f06a418fbd8c0bf9ec8a7bee18b3b2188ef20d8
Artyom Pervukhin <artyom@evasive.ru>
2019-05-09 14:17:52 +0300

    mdurlcheck: check any linked files, handle image links

--- mdurlcheck/main.go
+++ mdurlcheck/main.go
@@ -1,16 +1,19 @@
 // Program mdurlcheck checks whether given markdown file has any broken relative
-// links to other markdown files.
+// links to other files.
 //
-// It takes single .md file as an argument, then finds relative links to other
-// .md files in it and checks whether such files exist on the filesystem.
+// It takes single .md file as an argument, then finds relative links (including
+// image links) to other files in it and checks whether such files exist on the
+// filesystem.
 //
 // Provided with the following file:
 //
 // 	[Document 1](doc1.md), [document 2](doc2.md), and [another
 // 	one](dir/doc.md)
 //
-// The program will check whether files doc1.md, doc2.md and dir/doc.md exist on
-// disk, relative to the location of provided file.
+//	![program illustration](img/screenshot.jpg "Screenshot")
+//
+// The program will check whether files doc1.md, doc2.md, dir/doc.md, and
+// img/screenshot.jpg exist on disk, relative to the location of provided file.
 //
 // Program reports any errors on stderr and exits with non-zero exit code.
 //
@@ -27,7 +30,6 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/gomarkdown/markdown/ast"
 	"github.com/gomarkdown/markdown/parser"
@@ -56,22 +58,31 @@ func run(name string) error {
 		if !entering {
 			return ast.GoToNext
 		}
+		var dst string
 		switch n := node.(type) {
 		case *ast.Link:
-			u, err := url.Parse(string(n.Destination))
-			if err != nil {
-				log.Printf("%s: url %q parse: %v", name, n.Destination, err)
-				hadErrors = true
-				return ast.GoToNext
-			}
-			if u.Scheme != "" || u.Host != "" || !strings.HasSuffix(u.Path, ".md") {
-				return ast.GoToNext
-			}
-			dst := filepath.Join(filepath.Dir(name), filepath.FromSlash(u.Path))
-			if !fileExists(dst) {
-				hadErrors = true
-				log.Printf("%s: link to %q is broken", name, u)
-			}
+			dst = string(n.Destination)
+		case *ast.Image:
+			dst = string(n.Destination)
+		default:
+			return ast.GoToNext
+		}
+		if dst == "" {
+			log.Printf("%s: empty url", name)
+			return ast.GoToNext
+		}
+		u, err := url.Parse(dst)
+		if err != nil {
+			log.Printf("%s: %q: %v", name, dst, err)
+			hadErrors = true
+			return ast.GoToNext
+		}
+		if u.Scheme != "" || u.Host != "" || u.Path == "" {
+			return ast.GoToNext
+		}
+		if !fileExists(filepath.Join(filepath.Dir(name), filepath.FromSlash(u.Path))) {
+			hadErrors = true
+			log.Printf("%s: %q: broken link", name, dst)
 		}
 		return ast.GoToNext
 	}
